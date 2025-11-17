ec206dfd804947aef8d87e33243df21e20512a6c
bouhaba <maryam.bouhaba@outlook.com>
2025-11-17 15:10:56 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
