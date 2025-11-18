772ede8dd05f5d4fab0ade62d9e513f47fab8890
BS4002 <bakkousalma@gmail.com>
2025-11-18 19:30:38 +0100

    modification data.js

--- src/data/data.js
+++ src/data/data.js
@@ -5,100 +5,120 @@ const moviesData = [
     title: "Mad Max: Fury Road",
     year: 2015,
     category: "Action",
-    rating: 8.1,
     image: "img/action1.jpg",
+    imageDetails: "img/action1.jpg",
     video: "https://www.youtube.com/embed/hEJnMQG9ev8",
-    description: "A woman rebels against a tyrannical ruler in a post-apocalyptic desert."
+    duration: "2h",
+    actors: ["Tom Hardy", "Charlize Theron"],
+    description: "Une femme se rebelle contre un dirigeant tyrannique dans un désert post-apocalyptique."
   },
   {
     id: 2,
     title: "John Wick",
     year: 2014,
     category: "Action",
-    rating: 7.4,
     image: "img/action2.jpg",
+    imageDetails: "img/action2.jpg",
     video: "https://www.youtube.com/embed/2AUmvWm5ZDQ",
-    description: "A retired hitman seeks vengeance for the killing of his dog."
+    duration: "1h41",
+    actors: ["Keanu Reeves", "Michael Nyqvist"],
+    description: "Un ancien tueur à gages cherche vengeance après la mort de son chien."
   },
   {
     id: 3,
     title: "Gladiator",
     year: 2000,
     category: "Action",
-    rating: 8.5,
     image: "img/action3.jpg",
+    imageDetails: "img/action3.jpg",
     video: "https://www.youtube.com/embed/P5ieIbInFpg",
-    description: "A betrayed Roman general seeks justice against the corrupt emperor."
+    duration: "2h35",
+    actors: ["Russell Crowe", "Joaquin Phoenix"],
+    description: "Un général romain trahi cherche justice contre l'empereur corrompu."
   },
   {
     id: 4,
     title: "The Dark Knight",
     year: 2008,
     category: "Action",
-    rating: 9.0,
     image: "img/action4.jpg",
+    imageDetails: "img/action4.jpg",
     video: "https://www.youtube.com/embed/EXeTwQWrcwY",
-    description: "Batman faces the Joker, a criminal mastermind."
+    duration: "2h32",
+    actors: ["Christian Bale", "Heath Ledger"],
+    description: "Batman affronte le Joker, un criminel doté d'un esprit chaotique."
   },
   {
     id: 5,
     title: "Inception",
     year: 2010,
     category: "Action",
-    rating: 8.8,
     image: "img/action5.jpg",
+    imageDetails: "img/action5.jpg",
     video: "https://www.youtube.com/embed/YoHD9XEInc0",
-    description: "A thief enters people's dreams to steal information."
+    duration: "2h28",
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
+    description: "Un voleur infiltre les rêves des gens pour y dérober des informations secrètes."
   },
   {
     id: 6,
     title: "Avatar",
     year: 2009,
     category: "Action",
-    rating: 7.8,
     image: "img/action6.jpg",
+    imageDetails: "img/action6.jpg",
     video: "https://www.youtube.com/embed/5PSNL1qE6VY",
-    description: "A marine on an alien planet becomes part of its native tribe."
+    duration: "2h42",
+    actors: ["Sam Worthington", "Zoe Saldana"],
+    description: "Un marine paraplégique découvre une planète étrangère et s'attache à ses habitants."
   },
   {
     id: 7,
     title: "The Matrix",
     year: 1999,
     category: "Action",
-    rating: 8.7,
     image: "img/action7.jpg",
+    imageDetails: "img/action7.jpg",
     video: "https://www.youtube.com/embed/vKQi3bBA1y8",
-    description: "A hacker discovers the true nature of his reality."
+    duration: "2h16",
+    actors: ["Keanu Reeves", "Laurence Fishburne"],
+    description: "Un hacker découvre que la réalité qu’il connaît n’est qu’une simulation."
   },
   {
     id: 8,
     title: "Avengers: Endgame",
     year: 2019,
     category: "Action",
-    rating: 8.4,
     image: "img/action8.jpg",
+    imageDetails: "img/action8.jpg",
     video: "https://www.youtube.com/embed/TcMBFSGVi1c",
-    description: "The Avengers assemble to undo Thanos' destruction."
+    duration: "3h2",
+    actors: ["Robert Downey Jr.", "Chris Evans"],
+    description: "Les Avengers se réunissent pour tenter d'inverser les dégâts causés par Thanos."
   },
   {
     id: 9,
     title: "Mission Impossible: Fallout",
     year: 2018,
     category: "Action",
-    rating: 7.8,
     image: "img/action9.jpg",
+    imageDetails: "img/action9.jpg",
     video: "https://www.youtube.com/embed/wb49-oV0F78",
-    description: "Ethan Hunt is tested after a mission goes wrong."
+    duration: "2h27",
+    actors: ["Tom Cruise", "Henry Cavill"],
+    description: "Ethan Hunt affronte une menace grandissante après l'échec d’une mission."
   },
   {
     id: 10,
     title: "Fast & Furious 7",
     year: 2015,
     category: "Action",
-    rating: 7.1,
     image: "img/action10.jpg",
+    imageDetails: "img/action10.jpg",
     video: "https://www.youtube.com/embed/Skpu5HaVkOc",
-    description: "The crew faces a new threatening enemy."
+    duration: "2h17",
+    actors: ["Vin Diesel", "Paul Walker"],
+    description: "L’équipe doit faire face à un ennemi brillant et impitoyable."
   },
 
   // ===================== COMEDY =====================
@@ -107,100 +127,120 @@ const moviesData = [
     title: "The Mask",
     year: 1994,
     category: "Comedy",
-    rating: 6.9,
     image: "img/comedy1.jpg",
+    imageDetails: "img/comedy1.jpg",
     video: "https://www.youtube.com/embed/LZl69yk5lEY",
-    description: "A shy clerk becomes a wild superhero when he wears a magical mask."
+    duration: "1h41",
+    actors: ["Jim Carrey", "Cameron Diaz"],
+    description: "Un employé timide devient un personnage excentrique grâce à un masque magique."
   },
   {
     id: 12,
     title: "Home Alone",
     year: 1990,
     category: "Comedy",
-    rating: 7.6,
     image: "img/comedy2.jpg",
+    imageDetails: "img/comedy2.jpg",
     video: "https://www.youtube.com/embed/jEDaVHmw7r4",
-    description: "A boy defends his home from burglars during Christmas."
+    duration: "1h43",
+    actors: ["Macaulay Culkin", "Joe Pesci"],
+    description: "Un jeune garçon protège sa maison contre deux cambrioleurs pendant Noël."
   },
   {
     id: 13,
     title: "Johnny English",
     year: 2003,
     category: "Comedy",
-    rating: 6.2,
     image: "img/comedy3.jpg",
+    imageDetails: "img/comedy3.jpg",
     video: "https://www.youtube.com/embed/-oD2q3F6ybc",
-    description: "A clumsy secret agent tries to stop a dangerous criminal."
+    duration: "1h28",
+    actors: ["Rowan Atkinson", "John Malkovich"],
+    description: "Un agent secret maladroit tente d'arrêter un criminel dangereux."
   },
   {
     id: 14,
     title: "The Hangover",
     year: 2009,
     category: "Comedy",
-    rating: 7.7,
     image: "img/comedy4.jpg",
+    imageDetails: "img/comedy4.jpg",
     video: "https://www.youtube.com/embed/tcdUhdOlz9M",
-    description: "A bachelor party in Las Vegas goes terribly wrong."
+    duration: "1h48",
+    actors: ["Bradley Cooper", "Zach Galifianakis"],
+    description: "Une fête de célibataire à Las Vegas tourne au chaos total."
   },
   {
     id: 15,
     title: "Deadpool",
     year: 2016,
     category: "Comedy",
-    rating: 8.0,
     image: "img/comedy5.jpg",
+    imageDetails: "img/comedy5.jpg",
     video: "https://www.youtube.com/embed/ONHBaC-pfsk",
-    description: "A mercenary gains superpowers and seeks revenge."
+    duration: "1h48",
+    actors: ["Ryan Reynolds", "Morena Baccarin"],
+    description: "Un mercenaire au humour noir cherche à se venger après avoir été transformé."
   },
   {
     id: 16,
     title: "Jumanji",
     year: 2017,
     category: "Comedy",
-    rating: 6.9,
     image: "img/comedy6.jpg",
+    imageDetails: "img/comedy6.jpg",
     video: "https://www.youtube.com/embed/2QKg5SZ_35I",
-    description: "Four teens get trapped inside a magical video game."
+    duration: "1h59",
+    actors: ["Dwayne Johnson", "Kevin Hart"],
+    description: "Quatre adolescents se retrouvent piégés dans un jeu vidéo magique."
   },
   {
     id: 17,
     title: "Central Intelligence",
     year: 2016,
     category: "Comedy",
-    rating: 6.3,
     image: "img/comedy7.jpg",
+    imageDetails: "img/comedy7.jpg",
     video: "https://www.youtube.com/embed/MxEw3elSJ8M",
-    description: "A former geek turned CIA agent drags his old friend into danger."
+    duration: "1h47",
+    actors: ["Dwayne Johnson", "Kevin Hart"],
+    description: "Un ancien élève harcelé devenu agent de la CIA implique un ancien camarade."
   },
   {
     id: 18,
     title: "Rush Hour",
     year: 1998,
     category: "Comedy",
-    rating: 7.0,
     image: "img/comedy8.jpg",
+    imageDetails: "img/comedy8.jpg",
     video: "https://www.youtube.com/embed/JMiFsFQcFLE",
-    description: "A Hong Kong detective teams up with a loudmouthed LAPD officer."
+    duration: "1h38",
+    actors: ["Jackie Chan", "Chris Tucker"],
+    description: "Un détective de Hong Kong fait équipe avec un policier bavard de Los Angeles."
   },
   {
     id: 19,
     title: "Mr. Bean’s Holiday",
     year: 2007,
     category: "Comedy",
-    rating: 6.4,
     image: "img/comedy9.jpg",
+    imageDetails: "img/comedy9.jpg",
     video: "https://www.youtube.com/embed/9zzhbYV4xvM",
-    description: "Mr. Bean accidentally travels to France and causes chaos."
+    duration: "1h30",
+    actors: ["Rowan Atkinson", "Emma de Caunes"],
+    description: "Mr. Bean part accidentellement en vacances en France, provoquant des catastrophes."
   },
   {
     id: 20,
     title: "Yes Man",
     year: 2008,
     category: "Comedy",
-    rating: 6.8,
     image: "img/comedy10.jpg",
+    imageDetails: "img/comedy10.jpg",
     video: "https://www.youtube.com/embed/OHcFSmKmT74",
-    description: "A man decides to say yes to everything for a year."
+    duration: "1h44",
+    actors: ["Jim Carrey", "Zooey Deschanel"],
+    description: "Un homme décide de dire oui à absolument tout, transformant sa vie."
   },
 
   // ===================== DRAMA =====================
@@ -209,100 +249,120 @@ const moviesData = [
     title: "Forrest Gump",
     year: 1994,
     category: "Drama",
-    rating: 8.8,
     image: "img/drama1.jpg",
+    imageDetails: "img/drama1.jpg",
     video: "https://www.youtube.com/embed/bLvqoHBptjg",
-    description: "A simple man witnesses major historical events."
+    duration: "2h22",
+    actors: ["Tom Hanks", "Robin Wright"],
+    description: "Un homme simple d'esprit se retrouve mêlé à de grands événements historiques."
   },
   {
     id: 22,
     title: "The Shawshank Redemption",
     year: 1994,
     category: "Drama",
-    rating: 9.3,
     image: "img/drama2.jpg",
+    imageDetails: "img/drama2.jpg",
     video: "https://www.youtube.com/embed/PLl99DlL6b4",
-    description: "A man wrongly imprisoned forms a bond with another inmate."
+    duration: "2h22",
+    actors: ["Tim Robbins", "Morgan Freeman"],
+    description: "Un homme accusé à tort développe une amitié profonde avec un co-détenu."
   },
   {
     id: 23,
     title: "Titanic",
     year: 1997,
     category: "Drama",
-    rating: 7.9,
     image: "img/drama3.jpg",
+    imageDetails: "img/drama3.jpg",
     video: "https://www.youtube.com/embed/kVrqfYjkTdQ",
-    description: "A love story unfolds aboard the doomed Titanic."
+    duration: "3h14",
+    actors: ["Leonardo DiCaprio", "Kate Winslet"],
+    description: "Une histoire d'amour se développe à bord du Titanic, voué à sombrer."
   },
   {
     id: 24,
     title: "The Green Mile",
     year: 1999,
     category: "Drama",
-    rating: 8.6,
     image: "img/drama4.jpg",
+    imageDetails: "img/drama4.jpg",
     video: "https://www.youtube.com/embed/Ki4haFrqSrw",
-    description: "A prison guard discovers a prisoner with a mysterious gift."
+    duration: "3h9",
+    actors: ["Tom Hanks", "Michael Clarke Duncan"],
+    description: "Un gardien de prison découvre qu'un détenu possède un don mystérieux."
   },
   {
     id: 25,
     title: "The Pursuit of Happyness",
     year: 2006,
     category: "Drama",
-    rating: 8.0,
     image: "img/drama5.jpg",
+    imageDetails: "img/drama5.jpg",
     video: "https://www.youtube.com/embed/DMOBlEcRuw8",
-    description: "A struggling salesman fights for a better future."
+    duration: "1h57",
+    actors: ["Will Smith", "Jaden Smith"],
+    description: "Un vendeur en difficulté se bat pour offrir un avenir meilleur à son fils."
   },
   {
     id: 26,
     title: "A Star Is Born",
     year: 2018,
     category: "Drama",
-    rating: 7.6,
     image: "img/drama6.jpg",
+    imageDetails: "img/drama6.jpg",
     video: "https://www.youtube.com/embed/nSbzyEJ8X9E",
-    description: "A musician helps a young singer find fame."
+    duration: "2h16",
+    actors: ["Lady Gaga", "Bradley Cooper"],
+    description: "Un musicien aide une jeune chanteuse à gagner en notoriété."
   },
   {
     id: 27,
     title: "Joker",
     year: 2019,
     category: "Drama",
-    rating: 8.4,
     image: "img/drama7.jpg",
+    imageDetails: "img/drama7.jpg",
     video: "https://www.youtube.com/embed/zAGVQLHvwOY",
-    description: "A mentally struggling man becomes the infamous Joker."
+    duration: "2h2",
+    actors: ["Joaquin Phoenix", "Robert De Niro"],
+    description: "Un homme mentalement instable se transforme en l'iconique Joker."
   },
   {
     id: 28,
     title: "La La Land",
     year: 2016,
     category: "Drama",
-    rating: 8.0,
     image: "img/drama8.jpg",
+    imageDetails: "img/drama8.jpg",
     video: "https://www.youtube.com/embed/0pdqf4P9MB8",
-    description: "An actress and a pianist try to succeed in Los Angeles."
+    duration: "2h8",
+    actors: ["Ryan Gosling", "Emma Stone"],
+    description: "Une actrice et un pianiste tentent de réaliser leurs rêves à Los Angeles."
   },
   {
     id: 29,
     title: "Requiem for a Dream",
     year: 2000,
     category: "Drama",
-    rating: 8.3,
     image: "img/drama9.jpg",
+    imageDetails: "img/drama9.jpg",
     video: "https://www.youtube.com/embed/lgo3Hb5vWLE",
-    description: "Four people fall into drug addiction with tragic consequences."
+    duration: "1h42",
+    actors: ["Ellen Burstyn", "Jared Leto"],
+    description: "Quatre personnes sombrent dans l'addiction, entraînant des conséquences tragiques."
   },
   {
     id: 30,
     title: "The Wolf of Wall Street",
     year: 2013,
     category: "Drama",
-    rating: 8.2,
     image: "img/drama10.jpg",
+    imageDetails: "img/drama10.jpg",
     video: "https://www.youtube.com/embed/iszwuX1AK6A",
-    description: "A stockbroker rises and falls in the world of finance."
+    duration: "3h",
+    actors: ["Leonardo DiCaprio", "Jonah Hill"],
+    description: "L'histoire d'un courtier qui gravit les échelons avant de s'effondrer dans l'excès."
   },
 
   // ===================== FAMILY =====================
@@ -311,100 +371,120 @@ const moviesData = [
     title: "Frozen",
     year: 2013,
     category: "Family",
-    rating: 7.5,
     image: "img/family1.jpg",
+    imageDetails: "img/family1.jpg",
     video: "https://www.youtube.com/embed/TbQm5doF_Uc",
-    description: "Two sisters navigate magic, love, and adventure."
+    duration: "1h42",
+    actors: ["Idina Menzel", "Kristen Bell"],
+    description: "Deux sœurs affrontent magie, amour et aventure pour sauver leur royaume."
   },
   {
     id: 32,
     title: "Toy Story",
     year: 1995,
     category: "Family",
-    rating: 8.3,
     image: "img/family2.jpg",
+    imageDetails: "img/family2.jpg",
     video: "https://www.youtube.com/embed/v-PjgYDrg70",
-    description: "A cowboy doll feels threatened by a new spaceman toy."
+    duration: "1h21",
+    actors: ["Tom Hanks", "Tim Allen"],
+    description: "Un cow-boy en jouet se sent menacé par l'arrivée d'un nouveau jouet astronaute."
   },
   {
     id: 33,
     title: "Finding Nemo",
     year: 2003,
     category: "Family",
-    rating: 8.1,
     image: "img/family3.jpg",
+    imageDetails: "img/family3.jpg",
     video: "https://www.youtube.com/embed/wZdpNglLbt8",
-    description: "A clownfish searches the ocean for his lost son."
+    duration: "1h40",
+    actors: ["Albert Brooks", "Ellen DeGeneres"],
+    description: "Un poisson-clown traverse l'océan à la recherche de son fils perdu."
   },
   {
     id: 34,
     title: "Shrek",
     year: 2001,
     category: "Family",
-    rating: 7.9,
     image: "img/family4.jpg",
+    imageDetails: "img/family4.jpg",
     video: "https://www.youtube.com/embed/CwXOrWvPBPk",
-    description: "An ogre goes on a mission to rescue a princess."
+    duration: "1h35",
+    actors: ["Mike Myers", "Eddie Murphy"],
+    description: "Un ogre grincheux part sauver une princesse pour retrouver sa tranquillité."
   },
   {
     id: 35,
     title: "The Lion King",
     year: 1994,
     category: "Family",
-    rating: 8.5,
     image: "img/family5.jpg",
+    imageDetails: "img/family5.jpg",
     video: "https://www.youtube.com/embed/4sj1MT05lAA",
-    description: "A lion cub grows into a king after tragedy."
+    duration: "1h28",
+    actors: ["Matthew Broderick", "James Earl Jones"],
+    description: "Un lionceau doit accepter son destin pour devenir roi après une tragédie."
   },
   {
     id: 36,
     title: "Zootopia",
     year: 2016,
     category: "Family",
-    rating: 8.0,
     image: "img/family6.jpg",
+    imageDetails: "img/family6.jpg",
     video: "https://www.youtube.com/embed/jWM0ct-OLsM",
-    description: "A rabbit police officer teams up with a fox to solve a case."
+    duration: "1h48",
+    actors: ["Ginnifer Goodwin", "Jason Bateman"],
+    description: "Une lapine policière et un renard rusé unissent leurs forces pour résoudre une affaire."
   },
   {
     id: 37,
     title: "Moana",
     year: 2016,
     category: "Family",
-    rating: 7.6,
     image: "img/family7.jpg",
+    imageDetails: "img/family7.jpg",
     video: "https://www.youtube.com/embed/LKFuXETZUsI",
-    description: "A girl sets sail to save her island."
+    duration: "1h47",
+    actors: ["Auli'i Cravalho", "Dwayne Johnson"],
+    description: "Une jeune aventurière prend la mer pour sauver son île."
   },
   {
     id: 38,
     title: "Coco",
     year: 2017,
     category: "Family",
-    rating: 8.4,
     image: "img/family8.jpg",
+    imageDetails: "img/family8.jpg",
     video: "https://www.youtube.com/embed/Rvr68u6k5sI",
-    description: "A boy enters the land of the dead to learn about his family."
+    duration: "1h45",
+    actors: ["Anthony Gonzalez", "Gael García Bernal"],
+    description: "Un garçon explore le monde des morts pour découvrir l'histoire de sa famille."
   },
   {
     id: 39,
     title: "Up",
     year: 2009,
     category: "Family",
-    rating: 8.3,
     image: "img/family9.jpg",
+    imageDetails: "img/family9.jpg",
     video: "https://www.youtube.com/embed/HWEW_qTLSEE",
-    description: "A man goes on an adventure in a floating house with a young boy."
+    duration: "1h36",
+    actors: ["Edward Asner", "Jordan Nagai"],
+    description: "Un vieil homme part à l'aventure dans une maison volante accompagné d'un jeune garçon."
   },
   {
     id: 40,
     title: "Despicable Me",
     year: 2010,
     category: "Family",
-    rating: 7.6,
     image: "img/family10.jpg",
+    imageDetails: "img/family10.jpg",
     video: "https://www.youtube.com/embed/tVN9tYj1Ydw",
-    description: "A villain adopts three girls who change his life."
+    duration: "1h35",
+    actors: ["Steve Carell", "Miranda Cosgrove"],
+    description: "Un super-vilain voit sa vie changer en adoptant trois petites filles."
   }
 ];
 
