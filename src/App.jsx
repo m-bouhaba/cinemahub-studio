087f0b15617ae60f5f967ea30206468b3dbc8df0
bouhaba <maryam.bouhaba@outlook.com>
2025-11-19 09:25:01 +0100

    maryam

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
 
   return (
     <>
+<BrowserRouter>
+   <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    <Footer/>
 
+    </BrowserRouter>
     </>
   )
 }
