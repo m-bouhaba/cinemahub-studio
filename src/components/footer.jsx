fc3810c57933cc81b63a3d9cc72e0ccd14dd3898
BS4002 <bakkousalma@gmail.com>
2025-11-19 16:45:01 +0100

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>FOOTER</div>
+    <footer>
+      <div className='footerElement'>
+        <img src="/images/logoFooter.png" alt="logo" />
+        <p>Votre destination cinéma.<br/>Explorez les meilleurs films, genres et nouveautés.</p>
+        <div className='formFooter'>
+          <input type="text" placeholder='Votre Adresse mail' />
+          <button>S'abonner</button>
+        </div>
+      </div>
+      <div className='copyright'>
+        <p>© 2025 CineVerse. Tous droits réservés.</p>
+      </div>
+    </footer>
   )
 }
