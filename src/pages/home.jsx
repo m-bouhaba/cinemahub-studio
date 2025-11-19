df291073885f8eaa0f7d272592141d77e87cf419
bouhaba <maryam.bouhaba@outlook.com>
2025-11-19 21:59:34 +0100

    affichage de top10 section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import moviesData from '../data/data'
 
 export default function Home() {
     return (
         <>
-            <div className='heroSection'>
+            <section className='heroSection'>
                 <h1>
-                    <span class="red">D</span>écouvrez.
-                    <span class="red">R</span>egardez.
-                    <span class="red">V</span>ivez
+                    <span className="red">D</span>écouvrez.
+                    <span className="red">R</span>egardez.
+                    <span className="red">V</span>ivez
                 </h1>
                 <p>Votre portail vers un cinéma sans limites.</p>
 
-            </div>
+            </section>
+            <section className='top10Section'>
+                <h2> Top 10 du moment</h2>
+                <div className="cardsContainer">
+                {moviesData.slice(10, 20).map((movie, index) => (
+                    <div key={index} className="movieCard">
+                        <img src={movie.image} alt={movie.title} />
+                        <h1>{movie.title}</h1>
+                    </div>
+
+                ))}
+                </div>
+
+
+            </section >
 
 
         </>
