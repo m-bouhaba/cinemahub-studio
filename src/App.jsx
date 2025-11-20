fe56dd32b6f4899eb8d004458cc89cf8fa615cf8
BS4002 <bakkousalma@gmail.com>
2025-11-20 21:23:55 +0100

    fonctions de contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home';
 import Contact from './pages/contact';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import Popup from './components/popup';
 import Details from './pages/details';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Navbar/>
+    
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home/>} />
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,28 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [nom, setNom] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [feedback, setFeedback] = useState("");
+
+
+  const handleClick = () => {
+    // Vérifier champs vides
+    if (!nom || !email || !message) {
+      setFeedback("Tous les champs sont obligatoires.");
+    }
+    // Vérifier email simple
+    else if (!email.includes("@") || !email.includes(".")) {
+      setFeedback("Email doit avoir un format valide.");
+    }
+    // Succès
+    else {
+      setFeedback("Message envoyé avec succès !");
+    }
+  };
+
   return (
     <div className='contact'>
       <h1>Contact</h1>
+
       <div>
-        <form action="">
+        <form>
           <h2>Parlons cinéma ensemble.</h2>
+
           <label>
             <span>Nom</span>
-            <input type="text" name="" id="" />
+            <input 
+              type="text"
+              onChange={(e) => setNom(e.target.value)}
+            />
           </label>
+
           <label>
             <span>Email</span>
-            <input type="email" name="" id=""/>
+            <input 
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
+
           <label>
             <span>Message</span>
-            <textarea name="message" id=""></textarea>
+            <textarea
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </label>
-          <button>S'abonner</button>
-          <p></p>
+
+          <button type="button" onClick={handleClick}>
+            S'abonner
+          </button>
+
+          <p>{feedback}</p>
         </form>
       </div>
     </div>
-  )
+  );
 }
