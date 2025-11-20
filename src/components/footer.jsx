51cc60cb1f51074a49627cea5a9d3914a9c8bbfe
BS4002 <bakkousalma@gmail.com>
2025-11-20 17:05:38 +0100

    contact style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
-      <div className='footerElement'>
-        <img src="/images/logoFooter.png" alt="logo" />
-        <p>Votre destination cinéma.<br/>Explorez les meilleurs films, genres et nouveautés.</p>
-        <div className='formFooter'>
-          <input type="text" placeholder='Votre Adresse mail' />
-          <button>S'abonner</button>
+    <div className='footer'>
+      <footer>
+        <div className='footerElement'>
+          <img src="/images/logoFooter.png" alt="logo" />
+          <p>Votre destination cinéma.<br/>Explorez les meilleurs films, genres et nouveautés.</p>
+          <div className='formFooter'>
+            <input type="text" placeholder='Votre Adresse mail' />
+            <button>S'abonner</button>
+          </div>
         </div>
-      </div>
-      <div className='copyright'>
-        <p>© 2025 CineVerse. Tous droits réservés.</p>
-      </div>
-    </footer>
+        <div className='copyright'>
+          <p>© 2025 CineVerse. Tous droits réservés.</p>
+        </div>
+      </footer>
+    </div>
   )
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>
-      welcome contact
+    <div className='contact'>
+      <h1>Contact</h1>
+      <div>
+        <form action="">
+          <h2>Parlons cinéma ensemble.</h2>
+          <label>
+            <span>Nom</span>
+            <input type="text" name="" id="" />
+          </label>
+          <label>
+            <span>Email</span>
+            <input type="email" name="" id=""/>
+          </label>
+          <label>
+            <span>Message</span>
+            <textarea name="message" id=""></textarea>
+          </label>
+          <button>S'abonner</button>
+          <p></p>
+        </form>
+      </div>
     </div>
   )
 }
